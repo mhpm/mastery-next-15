a6bbd7c661fabe0e829bd4485ade1209aae617b5
Michelle Perez Morales <michelle.perez@sngular.com>
2024-11-14 14:20:03 -0600

    ✨ - font added stable version

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-10 font-[family-name:var(--font-geist-sans)]">
-      Hello
+      <h1>Hello</h1>
     </div>
   );
 }
+
+// export async function MyComponent() {
+//   const response = await fetch('https://jasonplaceholder.typicode.com/albums');
+//   const data = await response.json();
+
+//   return (
+//     <div>
+//       <h1>My Prefetch Data from server side Rendering</h1>
+//       {data.map((item: any) => (
+//         <div key={item.key}>{item.title}</div>
+//       ))}
+//     </div>
+//   );
+// }
