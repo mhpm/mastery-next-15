7f5a681e461ea231cc410e12eb18db48a432848d
Michelle Perez Morales <michelle.perez@sngular.com>
2024-12-22 23:46:53 -0600

    ⬆️ ✨ - sanity installed, stable, upgraded to ract 15 with react 19 stable

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,55 @@
-export default function Home() {
+import SearchForm from '@/components/SearchForm';
+import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
+import { STARTUPS_QUERY } from '@/sanity/lib/queries';
+import { sanityFetch, SanityLive } from '@/sanity/lib/live';
+import { auth } from '@/auth';
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const params = { search: query || null };
+
+  const session = await auth();
+  console.log('session: ', session);
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-10 font-[family-name:var(--font-geist-sans)]">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+
+      <SanityLive />
+    </>
   );
 }
-
-// export async function MyComponent() {
-//   const response = await fetch('https://jasonplaceholder.typicode.com/albums');
-//   const data = await response.json();
-
-//   return (
-//     <div>
-//       <h1>My Prefetch Data from server side Rendering</h1>
-//       {data.map((item: any) => (
-//         <div key={item.key}>{item.title}</div>
-//       ))}
-//     </div>
-//   );
-// }
